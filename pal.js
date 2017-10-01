ec226ec961bd71969db5e077e0d5c4a1b1758591
montefischer <monte.fischer@gmail.com>
2017-09-30 23:52:42 -0400

    Complete rewrite of the PALgorithm for Fall 2017. CHANGES: - Rewrote sorting algorithm to first assign each mentor/mentee a similarity coefficient, then iterate through mentees and select the first available mentor with the maximum similarity coefficient. - Added Pre-Professional Track sorting capability. - Updated major bins. - Added documentation to pal.js.

--- pal.js
+++ pal.js
@@ -1,17 +1,15 @@
+// I would suggest running this program as follows:
+// $ node pal.js > sorted.csv
+
 var Converter = require('csvtojson').Converter;
 var converter = new Converter({});
 
 converter.on("end_parsed", function(jsonArray){
-    
-    //split people into mentors and mentees
+
+    //split entries into mentors and mentees
     var mentors = [];
     var mentees = [];
-    var output = [];
-    var cleanUpMentees = [];
-    
-
     for(var i = 0; i < jsonArray.length; i++){
-
         if(jsonArray[i]["I\'m applying as a"] === "Mentor"){
             mentors.push(jsonArray[i]);
         }else{
@@ -19,166 +17,154 @@ converter.on("end_parsed", function(jsonArray){
         }
     }
 
-    console.log(mentors.length);
-    console.log(mentees.length);
+	// This sets up the array of similarity coefficients that the algorithm
+	// will fill. coeffarr[i] will be the array of all mentor similarity
+	// coefficients with the ith mentee. The algorithm prioritizes mentees who
+  // signed up first.
+	var coeffarr = new Array(mentees.length);
+	for(var menteeIndex = 0; menteeIndex < coeffarr.length; menteeIndex++) {
+		coeffarr[menteeIndex] = new Array(mentors.length);
+	}
+  assigncoeffarr(mentors, mentees, coeffarr);
 
+  // List of Mentors, will be ordered in optimal manner: mentee-to-mentee
+  // Currently assumes mentors <= mentees
+  var orderedMentors = []; //new Array(mentees.length);
+  for(var i = 0; i < mentees.length; i++) {
+    var maxCoeff = 0;
+    for(var j = 0; j < mentors.length; j++) {
+      if(coeffarr[i][j] > maxCoeff && orderedMentors.indexOf(mentors[j]) == -1) {
+        maxCoeff = coeffarr[i][j];
+      }
+    }
+    for (var j = 0; j < mentors.length; j++) {
+      if (coeffarr[i][j] === maxCoeff && orderedMentors.indexOf(mentors[j]) == -1) {
+        // Uncomment this line if you want to see the similarity coefficients
+        // console.log(mentees[i].Lname + " " + mentors[j].Lname + " " + maxCoeff);
+        orderedMentors.push(mentors[j]);
+        break;
+      }
+    }
+  }
 
-    algorithm(mentors, mentees, output, cleanUpMentees, false);    
-    console.log(mentors.length);
-    console.log(cleanUpMentees.length);
-    console.log(output.length);
+  // Spew the list of mentees out in .csv format
+  console.log("MENTEES");
+  for(var i = 0; i < mentees.length; i++) {
+    console.log(mentees[i].Fname + "," + mentees[i].Lname + "," + mentees[i].Major +
+    "," + mentees[i].Major2 + "," + mentees[i].Minor2 + "," + mentees[i].PP + "," +
+    mentees[i].Email + "," + mentees[i].Personality);
+  }
 
-    algorithm(mentors, cleanUpMentees, output, [], true);
-    console.log(mentors.length);
-    console.log(cleanUpMentees.length);
-
-    console.log(output.length);
-    
-    console.log(output);
-    
-
-
-
+  // Spew the list of mentors out in .csv format
+  console.log("MENTORS");
+  for(var i = 0; i < orderedMentors.length; i++) {
+    console.log(orderedMentors[i].Fname + "," + orderedMentors[i].Lname + "," +
+    orderedMentors[i].Major + "," + orderedMentors[i].Major2 + "," + orderedMentors[i].Minor2 + "," +
+    orderedMentors[i].PP + "," + orderedMentors[i].Email + "," + orderedMentors[i].Personality);
+  }
 });
 
-function algorithm(mentors, mentees, output, cleanUpMentees, cleanUp){
-    var pool = [];
-    var updatedPool = [];
-    
-    for(var i = 0; i < mentees.length; i++){
-        //console.log("i: " + i + " : length" + mentors.length);
-        pool = mentors.slice();
-        updatedPool = [];
+// Takes lists of mentors and mentees and a mentees.length by mentors.length
+// multidimensional array, and fills the array with similarity coefficients
+function assigncoeffarr(mentors, mentees, coeffarr) {
+	// Outer loop - mentees
+	for(var i = 0; i < mentees.length; i++) {
+		// Inner loop - mentors
+		for(var j = 0;  j < mentors.length; j++) {
+			// Similarity coefficient
+			var simcoeff = 0;
 
-        //search for exact first or second major match
-        for(var k = 0; k < mentors.length; k++){
-            
-            if(mentees[i].Major === mentors[k].Major){
-                //console.log("i: " + i + " " + mentees[i].Major + " " + mentors[k].Major);
-                updatedPool.push(mentors[k]);
-            }else if((mentees[i].Major === mentors[k]["Second Major"] || mentees[i]["Second Major"] === mentors[k].Major || mentees[i]["Second Major"] === mentors[k]["Second Major"]) && mentees[i]["Second Major"] !== ""){
-                updatedPool.push(mentors[k]);
-            }
+			// Pre-Professional track match -> +10 pts
+      // I count Graduate School as essentially None, but with a slight boost
+			if((mentees[i].PP == mentors[j].PP) ||
+         (mentees[i].PP == "na" && mentors[j].PP == "Graduate School") ||
+         (mentors[j].PP == "na" && mentees[j].PP == "Graduate School")){
+           if(mentees[i].PP == "Graduate School" && mentors[j].PP == "Graduate School") {
+             simcoeff += 2;
+           }
+        simcoeff += 10;
+			}
+
+      // Major or 2nd Major in same bin -> +3pts
+      var majorbinmatch = false;
+      for(var bin in binBin) {
+        // this could be simplified but I'm too tired. Sorry.
+        if(bin.indexOf(mentees[i].Major) != -1 && bin.indexOf(mentors[j].Major) != -1) {
+          majorbinmatch = true;
+          break;
         }
 
-        //no match has been found, go to major bins
-        if(updatedPool.length === 0){
-            
-            //find correct bin
-            var bin;
-            if(businessBin.indexOf(mentees[i].Major) > -1){
-                bin = businessBin.slice();
-            }else if(gradyBin.indexOf(mentees[i].Major) > -1){
-                bin = gradyBin.slice();
-            }else if(agricultureBin.indexOf(mentees[i].Major) > -1){
-                bin = agricultureBin.slice();
-            }else if(artBin.indexOf(mentees[i].Major) > -1){
-                bin = artBin.slice();
-            }else if(biologyBin.indexOf(mentees[i].Major) > -1){
-                bin = biologyBin.slice();
-            }else if(computerBin.indexOf(mentees[i].Major) > -1){
-                bin = computerBin.slice();
-            }else if(humanitiesBin.indexOf(mentees[i].Major) > -1){
-                bin = humanitiesBin.slice();
-            }else if(peopleBin.indexOf(mentees[i].Major) > -1){
-                bin = peopleBin.slice();
-            }else if(educationBin.indexOf(mentees[i].Major) > -1){
-                bin = educationBin.slice();
-            }else if(natureBin.indexOf(mentees[i].Major) > -1){
-                bin = natureBin.slice();
-            }else if(engineeringBin.indexOf(mentees[i].Major) > -1){
-                bin = engineeringBin.slice();
-            }else if(peopleBin.indexOf(mentees[i].Major) > -1){
-                bin = peopleBin.slice();
-            }else{
-                console.log("This should not be reached: " + mentees[i].Major);
-            }
-
-            for(var k = 0; k < mentors.length; k++){
-                if(bin.indexOf(mentors[k].Major) > -1){
-                    updatedPool.push(mentors[k]);
-                }
-            } 
+        if(bin.indexOf(mentees[i].Major2) != -1 && bin.indexOf(mentors[j].Major) != -1) {
+          majorbinmatch = true;
+          break;
         }
 
-
-        if(updatedPool.length === 1){
-            //console.log("i: " + i + " :" + mentees[i].Major);
-            output.push([mentees[i], updatedPool[0]]);
-            mentors.splice(mentors.indexOf(updatedPool[0]), 1);
-            continue;
-        }else if(updatedPool.length > 1){
-            pool = updatedPool.slice();
+        if(bin.indexOf(mentees[i].Major) != -1 && bin.indexOf(mentors[j].Major2) != -1) {
+          majorbinmatch = true;
+          break;
         }
 
-        updatedPool = [];
-
-        //move onto first or second minor match
-        for(var k = 0; k < pool.length; k++){
-            
-            if(mentees[i]["Minoring?"] === pool[k]["Minoring?"] && mentees[i]["Minoring?"] !== ""){
-                updatedPool.push(pool[k]);
-            }else if((mentees[i]["Minoring?"] === pool[k]["2nd Minor"] || mentees[i]["2nd Minor"] === pool[k]["Minoring?"] || mentees[i]["2nd Minor"] === pool[k]["2nd Minor"]) && mentees[i]["2nd Minor"] !== ""){
-                updatedPool.push(pool[k]);
-            }
+        if(bin.indexOf(mentees[i].Major2) != -1 && bin.indexOf(mentors[j].Major2) != -1) {
+          majorbinmatch = true;
+          break;
         }
+      }
+      if(majorbinmatch) {
+        simcoeff += 3;
+      }
 
-        if(updatedPool.length === 1){
-            output.push([mentees[i], updatedPool[0]]);
-            mentors.splice(mentors.indexOf(updatedPool[0]), 1);
-            continue;
-        }else if(updatedPool.length > 1){
-            pool = updatedPool.slice();
-        }
-        
-        
-        updatedPool = [];
+			// Major or 2nd Major direct match -> +4 pts
+			if(([mentees[i].Major, mentees[i].Major2].indexOf(mentors[j].Major) != -1 ) ||
+       ([mentees[i].Major, mentees[i].Major2].indexOf(mentors[j].Major2) != -1 && mentors[j].Major2 != '')) {
+				simcoeff += 4;
+			}
 
-        for(var k = 0; k < pool.length; k++){
-            
-            if(mentees[i]["Personality Profile"] === pool[k]["Personality Profile"]){
-                updatedPool.push(pool[k]);
-            }
-        }
+			// Minor or 2nd Minor direct match -> +2 pts
+			if(([mentees[i].Minor, mentees[i].Minor2].indexOf(mentors[j].Minor) != -1 ) ||
+       ([mentees[i].Minor, mentees[i].Minor2].indexOf(mentors[j].Minor2) != -1 && mentors[j].Minor2 != '')) {
+				simcoeff += 2;
+			}
 
-        if(updatedPool.length === 1){
-            output.push([mentees[i], updatedPool[0]]);
-            mentors.splice(mentors.indexOf(updatedPool[0]), 1);
-            continue;
-        }else if(updatedPool.length > 1){
-            pool = updatedPool.slice();
-        }
-        
+			// Personality match -> +1 pt
+			if(mentees[i].Personality == mentors[j].Personality) {
+				simcoeff += 1;
+			}
 
-        if(mentors.length === 0){
-            console.log("no more mentors");
-            break;
-        }
-
-        
-        //console.log("Person is at end with " + pool.length);
-
-        if(cleanUp){
-            output.push([mentees[i], pool[0]]);
-            mentors.splice(mentors.indexOf(pool[0]), 1);
-        }else{
-            cleanUpMentees.push(mentees[i]);
-        }
-    }
+			// Store similarity coefficient in coefficient array
+			coeffarr[i][j] = simcoeff;
+		}
+	}
 }
 
-
+// Major bins
 var businessBin = ["Accounting", "Accounting / International Business", "Actuarial Science Certificate", "Business & Political German Certificate", "Economics (A.B.)", "Economics (B.B.A.)", "Economics / International Business", "Environmental Economics & Management", "Finance", "Finance / International Business", "Financial Planning", "General Business (Griffin)", "General Business (Online)", "International Affairs", "International Business", "Legal Studies Certificate", "Management", "Management / International Business", "Management Info Systems / Int'l Business", "Management Information Systems", "Marketing", "Marketing / International Business", "Political Science", "Pre-Business", "Pre-Law", "Real Estate", "Real Estate / International Business", "Risk Management & Insurance", "Risk Mgmt & Insurance / Int'l Business"];
 var gradyBin = ["Advertising", "Communication Sciences & Disorders", "Communication Studies", "Entertainment and Media Studies", "Journalism", "New Media Certificate", "Pre-Journalism", "Public Relations", "Journalism - Visual Journalism"];
 var agricultureBin = ["Agribusiness", "Agribusiness Law Certificate", "Agricultural & Applied Economics", "Agricultural Communication", "Agricultural Education", "Agricultural Engineering", "Agriscience & Environmental Systems", "Agrosecurity Certificate", "Animal Health", "Animal Science", "Avian Biology", "Dairy Science", "Food Industry Marketing & Administration", "Food Science", "Horticulture", "Integrated Pest Management Certificate", "International Agriculture Certificate", "Local Food Systems Certificate", "Organic Agriculture Certificate", "Poultry Science"];
-var artBin = ["Art - Ceramics", "Art - Drawing", "Art - Fabric Design", "Art - Graphic Design", "Art - Jewelry & Metalwork", "Art - Painting", "Art - Photography", "Art - Printmaking", "Art - Scientific Illustration", "Art - Sculpture", "Art Education", "Art History", "Art X: Expanded Forms", "Furnishings & Interiors", "Interior Design", "Music", "Music Business Certificate", "Music Composition", "Music Performance", "Music Theory", "Music Therapy", "Studio Art", "Theatre", "Landscape Architecture", "Dance (A.B.)", "Dance (B.F.A.)"];
+var artBin = ["Art - Ceramics", "Art - Drawing", "Art - Fabric Design", "Art - Graphic Design", "Art - Jewelry & Metalwork", "Art - Painting", "Art - Photography", "Art - Printmaking", "Art - Scientific Illustration", "Art - Sculpture", "Art Education", "Art History", "Art X: Expanded Forms", "Furnishings & Interiors", "Interior Design", "Music", "Music Business Certificate", "Music Composition", "Music Performance", "Music Theory", "Music Therapy", "Studio Art", "Theatre", "Landscape Architecture", "Dance (A.B.)", "Dance (B.F.A.)", "Mass Media Arts"];
 var biologyBin = ["Biochemistry & Molecular Biology", "Biological Science", "Biology", "Cellular Biology", "Chemistry (B.S.)","Chemistry (B.S.Chem.)", "Environmental Chemistry", "Genetics", "Global Health Certificate", "Applied Biotechnology", "Microbiology", "Pharmaceutical Sciences", "Pharmacy", "Physics & Astronomy", "Plant Biology", "Pre-Dentistry", "Pre-Medicine", "Pre-Optometry", "Pre-Pharmacy", "Pre-Veterinary Medicine (B.S.)", "Pre-Veterinary Medicine (B.S.A.)", "Pre-Veterinary Medicine (B.S.F.R.)"];
 var computerBin = ["Cognitive Science", "Computer Science", "Computing Certificate", "Environmental Engineering", "Mathematics","Statistics", "Physics"];
 var humanitiesBin = ["Comparative Literature", "Film Studies", "History", "Honors Interdisc. Studies (A.B.)", "Honors Interdisc. Studies (B.S.)", "Honors Interdisc. Studies (B.S.A.)", "Honors Interdisc. Studies (B.S.F.C.S.)", "Interdisciplinary Studies (A.B.)", "Interdisciplinary Studies (A.B.)", "Interdisciplinary Studies (B.F.A.)", "Interdisciplinary Studies (B.S.)", "Interdisciplinary Writing Certificate","Linguistics", "Medieval Studies Certificate", "Native American Studies Certificate", "African American Studies", "African American Studies Certificate", "African Studies Certificate", "Anthropology", "Archaeological Sciences Certificate",  "Philosophy", "Pre-Theology", "Religion", "Women's Studies"];
-var cultureBin = ["Classical Culture", "Classical Languages","English","French", "German", "Germanic & Slavic Languages", "Greek","Chinese Language & Literature", "British & Irish Studies Certificate", "Italian", "Japanese Language & Literatures", "Latin American & Caribbean Studies", "Latin American & Caribbean Studies Cert.", "Arabic","Asian Studies Certificate","Romance Languages", "Russian", "Spanish"];
+var cultureBin = ["Classical Culture", "Classical Languages","English","French", "German", "Germanic & Slavic Languages", "Greek","Chinese Language & Literature", "British & Irish Studies Certificate", "Italian", "Japanese Language & Literatures", "Latin American & Caribbean Studies", "Latin American & Caribbean Studies Cert.", "Arabic","Asian Studies Certificate","Romance Languages", "Russian", "Spanish", "Chinese Language and Literature"];
 var peopleBin = ["Dietetics", "Disability Studies Certificate", "Consumer Economics", "Consumer Foods", "Consumer Journalism", "Criminal Justice", "Environmental Health Science", "Environmental Resource Science", "Exercise & Sport Science", "Family & Consumer Sciences Education", "Fashion Merchandising", "Health Promotion", "Housing Management and Policy", "Human Development and Family Science", "Leadership & Service Certificate", "Athletic Training", "Nutritional Sciences", "Personal & Org. Leadership Cert.", "Social Work", "Sociology", "Sport Management", "Turfgrass Management", "Psychology"];
 var educationBin = ["Early Childhood Education", "Educ. Psych & Instructional Tech Certif.", "English Education", "Health and Physical Education", "Mathematics / Mathematics Education", "Mathematics Education", "Middle School Education", "Music Education", "Science Education", "Special Education", "World Language Education", "Biology / Science Education", "English / English Education","History / Social Studies Education"];
 var natureBin = ["Ecology (A.B.)", "Ecology (B.S.)", "Fisheries & Wildlife", "Entomology", "Forestry", "Geographic Information Science Cert.", "Geography (A.B.)", "Geography (B.S.)", "Geology (A.B.)", "Geology (B.S.)", "Global Studies Certificate", "Community Forestry Certificate", "Natural Resource Recreation & Tourism", "Pre-Forest Resources", "Water & Soil Resources (B.S.E.S.)", "Water & Soil Resources (B.S.F.R.)", "Water Resources Certificate", "Coastal & Oceanographic Eng. Cert.", "Atmospheric Sciences", "Environmental Ethics Certificate"];
 var engineeringBin = ["Electrical and Electronics Engineering", "Engineering Physics Certificate", "Engineering Science Certificate", , "Computer Systems Engineering", "Computer Systems Engineering Certificate", "Civil Engineering", "Mechanical Engineering", "Biochemical Engineering", "Biological Engineering"];
 
-require('fs').createReadStream('/home/kyle/Downloads/b.csv').pipe(converter);
+// bin of bins, for ease of use
+var binBin = [];
+binBin.push(businessBin);
+binBin.push(gradyBin);
+binBin.push(agricultureBin);
+binBin.push(artBin);
+binBin.push(biologyBin);
+binBin.push(computerBin);
+binBin.push(humanitiesBin);
+binBin.push(cultureBin);
+binBin.push(peopleBin);
+binBin.push(educationBin);
+binBin.push(engineeringBin);
+
+// data.csv is the .csv file exported by Google Forms. Put it in the same directory as newpal.js
+// Currently, you need to change the field names in b.csv to:
+// Fname,Lname,Major,Major2,Minor,Minor2,PP,Email,Phone,Personality,"I\'m applying as a", Blank (leave blank)
+require('fs').createReadStream('data.csv').pipe(converter);
